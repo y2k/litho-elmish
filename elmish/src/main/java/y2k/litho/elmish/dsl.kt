7b608711f78f570a1c8a85fd419b0e77ddbdad32
y2k <itwisterlx@gmail.com>
2017-10-15 19:30:25 +0300

    add progress dsl

--- elmish/src/main/java/y2k/litho/elmish/dsl.kt
+++ elmish/src/main/java/y2k/litho/elmish/dsl.kt
@@ -2,6 +2,7 @@ package y2k.litho.elmish
 
 import com.facebook.litho.*
 import com.facebook.litho.ComponentLayout.ContainerBuilder
+import com.facebook.litho.widget.Progress
 import com.facebook.litho.widget.Recycler
 
 typealias Contextual<T> = (ComponentContext) -> T
@@ -30,4 +31,30 @@ fun recyclerBuilder(f: Recycler.Builder.() -> Unit): Contextual<Recycler.Builder
     return { context ->
         Recycler.create(context).apply(f)
     }
+}
+
+typealias LayoutFunc = ComponentLayout.Builder.() -> Unit
+typealias LayoutFuncCallback = (LayoutFunc) -> Unit
+
+fun progressL(b: Progress.Builder.(LayoutFuncCallback) -> Unit): Contextual<ComponentLayout.Builder> {
+    return { context ->
+        val builder = Progress.create(context)
+
+        var lf: LayoutFunc? = null
+        val lfCallback: LayoutFuncCallback = { lf = it }
+
+        builder
+            .also { it.b(lfCallback) }
+            .withLayout()
+            .also { lb -> lf?.let { lb.it() } }
+    }
+}
+
+fun ComponentLayout.ContainerBuilder.childWithLayout(
+    cb: Contextual<ComponentLayout.Builder>,
+    f: ComponentLayout.Builder.() -> Unit
+) {
+    val a = cb.invoke(innerContext)
+    a.f()
+    child(a.build())
 }
