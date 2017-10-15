1b7ea5c2ab35083c1252b9bc4d79dcee3cb0b8fd
y2k <itwisterlx@gmail.com>
2017-10-15 20:55:49 +0300

    temporary make innerContext public

--- elmish/src/main/java/com/facebook/litho/utils.kt
+++ elmish/src/main/java/com/facebook/litho/utils.kt
@@ -2,7 +2,7 @@ package com.facebook.litho
 
 import com.facebook.litho.ComponentLayout.ContainerBuilder
 
-internal val ContainerBuilder.innerContext: ComponentContext
+val ContainerBuilder.innerContext: ComponentContext
     get() = when (this) {
         is InternalNode -> context
         else -> TODO()
