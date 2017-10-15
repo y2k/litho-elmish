8a3eb7571e033329c7c86d4418f563c792ea6390
y2k <itwisterlx@gmail.com>
2017-10-15 22:31:46 +0300

    add dsl methods

--- elmish/src/main/java/y2k/litho/elmish/dsl.kt
+++ elmish/src/main/java/y2k/litho/elmish/dsl.kt
@@ -4,8 +4,19 @@ import com.facebook.litho.*
 import com.facebook.litho.ComponentLayout.ContainerBuilder
 import com.facebook.litho.widget.Progress
 import com.facebook.litho.widget.Recycler
+import com.facebook.litho.widget.Text
 
 typealias Contextual<T> = (ComponentContext) -> T
+
+fun textL(f: Text.Builder.() -> Unit): Contextual<ComponentLayout.Builder> =
+    { context -> Text.create(context).also(f).withLayout() }
+
+fun recyclerL(f: Recycler.Builder.() -> Unit): Contextual<ComponentLayout.Builder> =
+    { context -> Recycler.create(context).also(f).withLayout() }
+
+fun ComponentLayout.ContainerBuilder.childLayout(c: Contextual<ComponentLayout.Builder>) {
+    child(c(innerContext))
+}
 
 fun ContainerBuilder.childBuilder(cb: Contextual<Component.Builder<*, *>>) {
     child(cb.invoke(innerContext))
@@ -15,23 +26,14 @@ fun ContainerBuilder.child(cb: Contextual<ComponentLayout>) {
     child(cb.invoke(innerContext))
 }
 
-fun column(f: ContainerBuilder.() -> Unit): Contextual<ComponentLayout> {
-    return { context ->
-        Column.create(context).apply(f).build()
-    }
-}
+fun column(f: ContainerBuilder.() -> Unit): Contextual<ComponentLayout> =
+    { context -> Column.create(context).apply(f).build() }
 
-fun recycler(f: Recycler.Builder.() -> Unit): Contextual<ComponentLayout> {
-    return { context ->
-        Recycler.create(context).apply(f).buildWithLayout()
-    }
-}
+fun recycler(f: Recycler.Builder.() -> Unit): Contextual<ComponentLayout> =
+    { context -> Recycler.create(context).apply(f).buildWithLayout() }
 
-fun recyclerBuilder(f: Recycler.Builder.() -> Unit): Contextual<Recycler.Builder> {
-    return { context ->
-        Recycler.create(context).apply(f)
-    }
-}
+fun recyclerBuilder(f: Recycler.Builder.() -> Unit): Contextual<Recycler.Builder> =
+    { context -> Recycler.create(context).apply(f) }
 
 typealias LayoutFunc = ComponentLayout.Builder.() -> Unit
 typealias LayoutFuncCallback = (LayoutFunc) -> Unit
